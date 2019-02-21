2569971d6e72eaf102b7f5da35596aefe845533f
Roides Javier Cruz Lara <roidescruzlara@gmail.com>
2019-02-21 13:53:39 -0600

    add Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from '../../logo.svg';
+import './Logo.css';
+
+export default function Logo() {
+  return (
+    <img src={logo} className="logo" alt="logo"/>
+  )
+}
