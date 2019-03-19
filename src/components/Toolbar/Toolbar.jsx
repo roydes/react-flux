f419b04593d5c03f134101a2c3c5f75e600d84ef
Roides Javier Cruz Lara <roidescruzlara@gmail.com>
2019-03-19 13:32:09 -0600

    add control for hide and show nav bar using flux data flow

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { PureComponent } from 'react'
 import Logo from '../Logo/Logo.jsx'
 import { Link } from 'react-router-dom'
-export default function Toolbar() {
-  return (
+import LayoutStore from '../../stores/LayoutStore'
+import * as LayoutActions from '../../actions/LayoutActions'
+import './Toolbar.css'
+
+export default class Toolbar extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = { sidenavVisibility: LayoutStore.getNavbarVisivility() }
+    this.sidenavVisibilityChange = this.sidenavVisibilityChange.bind(this)
+  }
+
+  sidenavVisibilityChange() {
+    LayoutActions.changeSidenavVisisbility(!this.state.sidenavVisibility)
+    this.setState({ sidenavVisibility: !this.state.sidenavVisibility })
+  }
+
+
+  render() {
+    return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow">
+        <button className="sidebar-controler" type="button" onClick = {this.sidenavVisibilityChange}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <Link className="navbar-brand" to="/">
           <Logo></Logo>
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
@@ -28,5 +46,6 @@ export default function Toolbar() {
           </form>
         </div>
       </nav>
-  )
+    )
+  }  
 }
